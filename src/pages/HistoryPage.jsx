35732f9cbd819292205ed5bcc39b3f77471659cd
salin <l.salin88@gmail.com>
2023-06-22 10:33:08 +0700

    update history logic search

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useEffect } from 'react'
-// import DatePicker from 'react-datepicker'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 // import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 // import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
@@ -24,7 +25,7 @@ export default function History() {
         {
             id: 2,
             title: 'แผนก ฉุกเฉิน',
-            jobType: 'part-time',
+            jobType: 'full-time',
             startDate: '18/12/2021',
             endDate: '18/12/2021',
             startTime: '10:00:00',
@@ -45,7 +46,7 @@ export default function History() {
             startTime: '10:00:00',
             endTime: '22:00:00',
             wage: 15000,
-            status: 'Active',
+            status: 'Inactive',
             statusDetail: 'รอการตอบรับ',
             description: 'รายละเอียดงาน xxxxxx',
             deleted: false,
@@ -60,7 +61,7 @@ export default function History() {
             startTime: '10:00:00',
             endTime: '22:00:00',
             wage: 15000,
-            status: 'Active',
+            status: 'Inactive',
             statusDetail: 'รอการตอบรับ',
             description: 'รายละเอียดงาน xxxxxx',
             deleted: false,
@@ -96,104 +97,161 @@ export default function History() {
     // const adapter = new AdapterDayjs()
     // const [startDate, setStartDate] = useState(adapter.date('01/01/2023'))
     // const [endDate, setEndDate] = useState(adapter.date(newDate))
-    const [startDate, setStartDate] = useState('')
-    const [endDate, setEndDate] = useState('')
+    const [startDate, setStartDate] = useState(new Date('01/01/2023'))
+    const [endDate, setEndDate] = useState(newDate)
 
     const [fullTimeChk, setFullTimeChk] = useState('checked')
     const [pathTimeChk, setPathTimeChk] = useState('checked')
     const [activeChk, setActiveChk] = useState('checked')
-    const [inactiveChk, setInactiveChk] = useState('checked')
+    const [sortChk, setSortChk] = useState('checked')
 
-    const [inputSearch, setInputSearch] = useState(null)
+    const [inputSearch, setInputSearch] = useState('')
+    const [showHistory, setShowHistory] = useState([])
 
-    const pastTime = (create_add, today) => {
+    // const pastTime = (create_add, today) => {
+    //     const oneDay = 24 * 60 * 60 * 1000
+    //     const arrCreate = create_add.split('/')
+    //     const arrToday = today.split('/')
+
+    //     const firstDate = new Date(arrCreate[2], arrCreate[1], arrCreate[0])
+    //     const secondDate = new Date(arrToday[2], arrToday[1], arrToday[0])
+    //     const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay))
+    //     if (diffDays == 0) {
+    //         return 'now'
+    //     } else {
+    //         if (diffDays > 30) {
+    //             return create_add
+    //         } else {
+    //             return `${diffDays}d`
+    //         }
+    //     }
+    // }
+    const diffDay = (start, end) => {
         const oneDay = 24 * 60 * 60 * 1000
-        const arrCreate = create_add.split('/')
-        const arrToday = today.split('/')
+        const arrCreate = start.split('/')
+        const arrToday = end.split('/')
 
         const firstDate = new Date(arrCreate[2], arrCreate[1], arrCreate[0])
         const secondDate = new Date(arrToday[2], arrToday[1], arrToday[0])
-        const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay))
-        if (diffDays == 0) {
+        const diffDays = Math.round((secondDate - firstDate) / oneDay)
+
+        return diffDays
+    }
+    const textDate = (hrs, date) => {
+        if (hrs == 0) {
             return 'now'
         } else {
-            if (diffDays > 30) {
-                return create_add
+            if (hrs > 30) {
+                return date
             } else {
-                return `${diffDays}d`
+                return `${hrs}d`
             }
         }
     }
+
     const objFilter = {
-        startDate: startDate,
-        endDate: endDate,
+        startDate: new Date(startDate).toLocaleDateString('en-GB'),
+        endDate: new Date(endDate).toLocaleDateString('en-GB'),
         fullTimeChk: fullTimeChk,
         pathTimeChk: pathTimeChk,
         activeChk: activeChk,
-        inactiveChk: inactiveChk,
+        sortChk: sortChk,
         inputSearch: inputSearch,
+        diffDay: diffDay(
+            new Date(startDate).toLocaleDateString('en-GB'),
+            new Date(endDate).toLocaleDateString('en-GB')
+        ),
     }
     const hdlFtOnChange = () => {
         console.log('hdlFtOnChange')
+
         fullTimeChk == 'checked'
             ? setFullTimeChk('')
             : setFullTimeChk('checked')
-        console.log(objFilter)
     }
     const hdlPtOnChange = () => {
         console.log('hdlPtOnChange')
         pathTimeChk == 'checked'
             ? setPathTimeChk('')
             : setPathTimeChk('checked')
-        console.log(objFilter)
     }
     const hdlActiveOnChange = () => {
         console.log('hdlActiveOnChange')
         activeChk == 'checked' ? setActiveChk('') : setActiveChk('checked')
-        console.log(objFilter)
     }
-    const hdlInactiveOnChange = () => {
-        console.log('hdlInactiveOnChange')
-        inactiveChk == 'checked'
-            ? setInactiveChk('')
-            : setInactiveChk('checked')
-        console.log(objFilter)
+    const hdlSortOnChange = () => {
+        console.log('hdlSortOnChange')
+        sortChk == 'checked' ? setSortChk('') : setSortChk('checked')
     }
+
     const hdlSearchOnChange = (event) => {
-        console.log(event.target.value)
-        console.log(objFilter)
         setInputSearch(event.target.value)
     }
+
+    const filter = (objFilter) => {
+        const arrayFP = arrHistory.filter((item) => {
+            if (objFilter.fullTimeChk == 'checked') {
+                if (item.jobType == 'full-time') {
+                    return item
+                }
+            }
+            if (objFilter.pathTimeChk == 'checked') {
+                if (item.jobType == 'part-time') {
+                    return item
+                }
+            }
+        })
+        const arrayActive = arrayFP.filter((item) => {
+            if (objFilter.activeChk == 'checked') {
+                if (item.status == 'Active') {
+                    return item
+                }
+            } else {
+                if (item.status == 'Inactive') {
+                    return item
+                }
+            }
+        })
+
+        if (objFilter.sortChk == 'checked') {
+            const x = arrayActive.sort((a, b) => {
+                return diffDay(b.create_at, today) - diffDay(a.create_at, today)
+            })
+            setShowHistory(x)
+        } else {
+            const x = arrayActive.reverse((a, b) => {
+                return diffDay(a.create_at, today) - diffDay(b.create_at, today)
+            })
+            setShowHistory(x)
+        }
+
+        setShowHistory(arrayActive)
+    }
+
+    useEffect(() => {
+        filter(objFilter)
+        console.log(objFilter)
+    }, [fullTimeChk, pathTimeChk, activeChk, sortChk, inputSearch])
 
     return (
         <div className=" bg-gray-200  w-[100vw] h-[100vh] flex flex-col justify-start items-center">
             <div className="bg-blue-400 w-[80%] mt-3 p-2 flex flex-row justify-between items-center">
-                <div className=" flex flex-row items-center  gap-5 ">
-                    {/* <DatePicker
-                        inputPlaceholder="Select a day"
-                        value={startDate}
-                        onChange={(newValue) => {
-                            console.log(newValue?.format('DD/MM/YYYY'))
-                            setStartDate(newValue)
-                            console.log(objFilter)
-                        }}
-                        locale="th"
-                        formatInputText={(value) => value.format('DD/MM/YYYY')}
-                        format="DD/MM/YYYY"
-                    />
-                    <h1 className="text-black">--</h1>
+                <div className=" flex flex-row items-center  ">
                     <DatePicker
-                        inputPlaceholder="Select a day"
-                        value={endDate}
-                        onChange={(newValue) => {
-                            console.log(newValue?.format('DD/MM/YYYY'))
-                            setEndDate(newValue)
-                            console.log(objFilter)
+                        selected={startDate}
+                        onChange={(date) => {
+                            // const d = new Date(date).toLocaleDateString('fr-FR');
+                            // console.log(d);
+                            setStartDate(date)
                         }}
-                        locale="th"
-                        formatInputText={(value) => value.format('DD/MM/YYYY')}
-                        format="DD/MM/YYYY"
-                    /> */}
+                        dateFormat={'dd/MM/yyyy'}
+                    />
+                    <h1 className="text-black ml-6 mr-6">-To-</h1>
+                    <DatePicker
+                        selected={endDate}
+                        onChange={(date) => setEndDate(date)}
+                        dateFormat={'dd/MM/yyyy'}
+                    />
                 </div>
                 <input
                     type="text"
@@ -224,51 +282,35 @@ export default function History() {
                         />
                         <span className="label-text">PT</span>
                     </label>
-                    <label className="label cursor-pointer  gap-3">
-                        <input
-                            type="checkbox"
-                            checked={activeChk}
-                            className="checkbox"
-                            onChange={hdlActiveOnChange}
-                        />
-                        <span className="label-text">Active</span>
-                    </label>
 
                     <label className="label cursor-pointer  gap-3">
                         <input
                             type="checkbox"
-                            checked={inactiveChk}
-                            className="checkbox"
-                            onChange={hdlInactiveOnChange}
+                            className="toggle toggle-success"
+                            checked={activeChk}
+                            onChange={hdlActiveOnChange}
                         />
-                        <span className="label-text">Inactive</span>
+                        <span className="label-text">Active</span>
                     </label>
-                    {/* <div onChange={() => {}}>
-                        <label className="label cursor-pointer  gap-3">
-                            <input
-                                type="radio"
-                                name="radio"
-                                className="radio radio-accent"
-                            />
-                            <span className="label-text">Active</span>
-                        </label>
-                        <label className="label cursor-pointer  gap-3">
-                            <input
-                                type="radio"
-                                name="radio"
-                                className="radio radio-accent"
-                            />
-                            <span className="label-text">Inactive</span>
-                        </label>
-                    </div> */}
+                    <label className="label cursor-pointer  gap-3">
+                        <input
+                            type="checkbox"
+                            className="toggle toggle-success"
+                            checked={sortChk}
+                            onChange={hdlSortOnChange}
+                        />
+                        <span className="label-text">Sort</span>
+                    </label>
                 </div>
                 <div>
                     <button className="btn btn-primary">Add</button>
                 </div>
             </div>
-            <div className="bg-red-200 w-[80%] h-[650px] mb-3 p-2">
+            <div className="bg-red-200 w-[80%] h-[650px] mb-3 p-2 text-black">
                 <h1>history</h1>
-                {arrHistory.map((item, index) => {
+                {showHistory.map((item, index) => {
+                    const dh = diffDay(item.create_at.split(' ')[0], today)
+
                     return (
                         <>
                             <div className="flex flex-row  bg-green-300 m-2 p-1 gap-2">
@@ -285,12 +327,10 @@ export default function History() {
                                 </div>
                                 <div>
                                     [
-                                    {pastTime(
-                                        item.create_at.split(' ')[0],
-                                        today
-                                    )}
+                                    {textDate(dh, item.create_at.split(' ')[0])}
                                     ]
                                 </div>
+                                <div>[{item.status}]</div>
                             </div>
                         </>
                     )
