3f0ff67e3d9db9a9f37d6de55a687376a3de99af
Arvenny8 <128786397+Arvenny8@users.noreply.github.com>
2023-06-22 21:12:12 +0700

    addpost update

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,45 +1,48 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RedirectIfAuthenticated from "../features/auth/auth-components/RedirectIfAuthenticated";
-import ProtectedRoute from "../features/auth/auth-components/ProtectedRoute";
-import Container from "../layouts/Container";
-import HomePage from "../pages/HomePage";
-import ProfilePage from "../pages/ProfilePage";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RedirectIfAuthenticated from '../features/auth/auth-components/RedirectIfAuthenticated'
+import ProtectedRoute from '../features/auth/auth-components/ProtectedRoute'
+import Container from '../layouts/Container'
+import HomePage from '../pages/HomePage'
+import ProfilePage from '../pages/ProfilePage'
+import AddPostPage from '../features/addpost/Addpost'
+import PagePost from '../pages/PagePost'
+import Addpost from '../features/addpost/Addpost'
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      //   <ProtectedRoute>
-      <Container />
-      //   </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/profile",
-        element: <ProfilePage />,
-      },
-      {
-        path: "/addpost",
-        element: <AddPostPage />,
-      },
-      {
-        path: "/login",
-        element: <LogInPage />,
-      },
-      {
-        path: "/register",
-        element: <RegisterPage />,
-      },
-      {
-        path: "/history",
-        element: <HistoryPage />,
-      },
-    ],
-  },
-]);
+    {
+        path: '/',
+        element: (
+            //   <ProtectedRoute>
+            <Container />
+            //   </ProtectedRoute>
+        ),
+        children: [
+            // {
+            //     path: "/",
+            //     element: <HomePage />,
+            // },
+            // {
+            //     path: "/profile",
+            //     element: <ProfilePage />,
+            // },
+            {
+                path: '/addpost',
+                element: <Addpost />,
+            },
+            // {
+            //     path: "/login",
+            //     element: <LogInPage />,
+            // },
+            // {
+            //     path: "/register",
+            //     element: <RegisterPage />,
+            // },
+            // {
+            //     path: "/history",
+            //     element: <HistoryPage />,
+            // },
+        ],
+    },
+])
 export default function Router() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />
 }
