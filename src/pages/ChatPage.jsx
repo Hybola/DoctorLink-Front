aa354076c61107c46eac774df22476eca380c3b1
Hybola <keeratipong.b@gmail.com>
2023-06-26 18:48:31 +0700

    initial

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -16,21 +16,15 @@ export default function ChatPage() {
             setSocketId(socket.id)
             setIsConnected(true)
         }
-        function onDisconnect() {
-            setIsConnected(false)
-        }
-
         function onGetMessage(roomMsg) {
             console.log('getMessage', roomMsg)
             setAllMsg(roomMsg)
         }
         socket.on('connect', onConnect)
-        socket.on('disconnect', onDisconnect)
         socket.on('getMessage', onGetMessage)
 
         return () => {
             socket.off('connect')
-            socket.off('disconnect')
             socket.off('getMessage')
         }
     }, [])
